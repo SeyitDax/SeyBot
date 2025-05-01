f7083cbb688ea2d272e57ca161f449b58c29382f
Seyit Ahmet <ahmetsaddemir@gmail.com>
2025-05-01 15:38:21 +0300

    Fixed .gitignore

--- Dialogs/NameCaptureDialog.cs
+++ Dialogs/NameCaptureDialog.cs
@@ -16,12 +16,39 @@ public class NameCaptureDialog : ComponentDialog
     {
         _cluHelper = cLUHelper;
 
-        AddDialog(new WaterfallDialog(nameof(WaterfallDialog)), new WaterfallStep[]
+        AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
         {
-
-        });
+            AskNameStepAsync,
+            ValidateNameStepAsync
+        }));
 
         AddDialog(new TextPrompt(nameof(TextPrompt)));
         InitialDialogId = (nameof(WaterfallDialog));
+    }
+    
+
+    private async Task<DialogTurnResult> AskNameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        return await stepContext.PromptAsync(nameof(TextPrompt),
+            new PromptOptions { Prompt = MessageFactory.Text("May I have your name before we proceed?") }, cancellationToken);
+    }
+
+    private async Task<DialogTurnResult> ValidateNameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        var input = (string)stepContext.Result;
+        var result = await _cluHelper.ExtractUserDetailsAsync(input); 
+        result.TryGetValue("PersonName", out string name);
+        result.TryGetValue("Intent", out string intent);
+
+        name ??= (intent == "Rejection") ? "friend" : null;
+
+        if(string.IsNullOrEmpty(name))
+        {
+           await stepContext.Context.SendActivityAsync("Sorry, I didn't catch that.");
+            return await stepContext.ReplaceDialogAsync(nameof(NameCaptureDialog), null, cancellationToken);
+        }
+
+        stepContext.Values["UserName"] = name;
+        return await stepContext.EndDialogAsync(name, cancellationToken);
     }
 }
